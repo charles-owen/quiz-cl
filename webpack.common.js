28530e6699f164669b59ae404ec1bd5c92b47682
cbowen <cbowen@cse.msu.edu>
2019-01-06 13:05:02 -0500

    New cl-publish system.

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,7 @@
 const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
 	entry: {
 		Quiz: path.resolve(__dirname, 'index.js')
-	},
-	plugins: [
-		new FileManagerPlugin({
-			onEnd: {
-				copy: [
-					{source: path.resolve(__dirname, '../../../cl/dist/quiz.*'), destination: path.resolve(__dirname, 'dist') }
-				]
-			}
-		})
-	]
+	}
 }
